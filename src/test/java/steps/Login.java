da1d7e65b872a5c7ddbc774bdc69dfc52d48b5a9
NAGARJUNREDDY\nagar <nagarjun.sdet@gmail.com>
2024-02-28 10:53:51 +0530

    login

--- src/test/java/steps/Login.java
+++ src/test/java/steps/Login.java
@@ -1,9 +1,16 @@
 package steps;
 
+import io.cucumber.java.en.Given;
+
 public class Login {
 	
 	public void setup() {
 		System.out.println("setup method");
+	}
+	
+	@Given("I launch the application in chrome browser")
+	public void launchApp() {
+		System.out.println("application is launched");
 	}
 
 }
